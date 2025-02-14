96ad70aeca01545eafdbac8720dfc75da6da6cb4
Micah <micahyy@qq.com>
2025-02-14 23:18:32 +0800

    no message

--- czmao/08gamer_atr2/config.h
+++
@@ -1,9 +0,0 @@
-#define DYNAMIC_KEYMAP_LAYER_COUNT 8 //定义VIA层数
-
-#ifdef ENCODER_ENABLE 
-# define ENCODERS_PAD_A     { D2 }
-#define  ENCODERS_PAD_B     { D3 }
-#define ENCODER_RESOLUTIONS { 4 }/** 旋钮步进，QMK BUG 不要设置 ENCODER_RESOLUTION */
-#define ENCODERS_MATRIX_MAP {{{4, 5}, {5, 5}}}
-// #   define ENCODERS_TYPE_EC11
-#endif
